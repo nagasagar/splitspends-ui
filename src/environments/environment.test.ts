e0bab6bd5a782cbb8d3c94755afdf714036a5fa9
nagasagar <nagasagar@gmail.com>
2019-11-09 20:37:16 +0100

    use https url of. heroku to avoid mixed content error / adjust facebook redirect to use https /  fix routing in netlify with_redirects

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   name: 'test',
   serverUrl: 'https://test.splitspends.com',
-  apiUrl: 'http://test.api.splitspends.com',
+  apiUrl: 'https://splitspends.herokucom',
   accessToken: 'accessToken',
   oAuth2RedirectUrl: 'https://test.splitspends.com/oauth2/redirect',
   googleAuthUrl: 'http://test.api.splitspends.com/oauth2/authorize/google?redirect_uri=https://test.splitspends.com/oauth2/redirect',
-  facebookAuthUrl: 'http://test.api.splitspends.com/oauth2/authorize/facebook?redirect_uri=https://test.splitspends.com/oauth2/redirect'
+  facebookAuthUrl: 'https://splitspends.herokucom/oauth2/authorize/facebook?redirect_uri=https://test.splitspends.com/oauth2/redirect'
 };
