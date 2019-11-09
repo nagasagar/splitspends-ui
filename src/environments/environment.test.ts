37d6a8f47e7fbb5d0818827b889bdfe943e3224a
nagasagar <nagasagar@gmail.com>
2019-11-09 17:53:19 +0100

    created a test environment for deployment to netlify

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  name: 'test',
+  serverUrl: 'https://test.splitspends.com',
+  apiUrl: 'https://splitspends.herokuapp.com',
+  accessToken: 'accessToken',
+  oAuth2RedirectUrl: 'https://test.splitspends.com/oauth2/redirect',
+  googleAuthUrl: 'https://splitspends.herokuapp.com/oauth2/authorize/google?redirect_uri=https://test.splitspends.com/oauth2/redirect',
+  facebookAuthUrl: 'https://splitspends.herokuapp.com/oauth2/authorize/facebook?redirect_uri=https://test.splitspends.com/oauth2/redirect'
+};
